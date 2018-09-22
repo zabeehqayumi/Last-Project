544b8550caf89a75226005ec7ce9c3d66a0abd04
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-21 21:27:34 -0400

    Initial Commit

---
+++ Light DJ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light DJ
+//
+//  Created by Zabeehullah Qayumi on 9/21/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
