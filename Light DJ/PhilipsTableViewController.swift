ee29d21a2c55c42ba10c5fa3aebeefce6efd875b
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-22 16:02:37 -0400

    designed Large Nav Bar

---
+++ Light DJ/PhilipsTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PhilipsTableViewController.swift
+//  Light DJ
+//
+//  Created by Zabeehullah Qayumi on 9/22/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class PhilipsTableViewController: UITableViewController {
+    
+    var arr = ["1","3","4"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     
+
+        //setNavigationBar()
+        
+
+
+    }
+    
+
+   
+
+    // MARK: - Table view data source
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return arr.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = arr[indexPath.row]
+        
+        return cell
+    }
+
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
