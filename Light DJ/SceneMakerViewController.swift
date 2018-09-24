1fdd8b18ff39267a1f35944892e3e30a45d6dcb8
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-24 19:47:01 -0400

    Visulizer, Scene Maker and Matrix are complete with topThreeButtons dropDown

--- Light DJ/SceneMakerViewController.swift
+++ Light DJ/SceneMakerViewController.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class SceneMakerViewController: UIViewController {
+    
+    
+    @IBOutlet var topThreeButtons: [UIButton]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func visualizerButtonPressed(_ sender: Any) {
+        
+        topThreeButtons.forEach { (Button) in
+            UIView.animate(withDuration: 0.2, animations: {
+                Button.isHidden = !Button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
     }
-    */
-
 }
