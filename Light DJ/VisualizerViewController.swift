bdbb4983294f15b94a433c148bdc4ac3408d28df
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-25 22:43:48 -0400

    Array of colors created and shuffled in the boxes.

--- Light DJ/VisualizerViewController.swift
+++ Light DJ/VisualizerViewController.swift
@@ -10,20 +10,38 @@ import UIKit
 
 class VisualizerViewController: UIViewController {
     
+    var randomIndex1 : Int = 0
+    var randomIndex2 : Int = 0
+    var randomIndex3 : Int = 0
+    
+    
+    @IBOutlet weak var colorBox1: UIView!
+    
+    @IBOutlet weak var colorBox2: UIView!
+    
+    @IBOutlet weak var colorBox3: UIView!
+    
+    
+    // array of colors
+    
+    var arrOfColors = [UIColor]()
+    
     @IBOutlet var topThreeButtons: [UIButton]!
     
     @IBOutlet var mellowEffects: [UIButton]!
     
     @IBOutlet var activeEffects: [UIButton]!
     
-    
-    
-    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addingColorsForArray()
+        cornerRadius()
+        
+
+
     }
     
     
@@ -100,6 +118,53 @@ class VisualizerViewController: UIViewController {
         
         
     }
+    
+    
+    @IBAction func suffleButtonPressed(_ sender: Any) {
+        //Calling
+        updateColorBoxes()
+        
+    }
+    
+    func updateColorBoxes(){
+        
+        randomIndex1 = Int(arc4random_uniform(8))
+        randomIndex2 = Int(arc4random_uniform(8))
+        randomIndex3 = Int(arc4random_uniform(8))
+        
+        
+        colorBox1.backgroundColor = arrOfColors[randomIndex1]
+        colorBox2.backgroundColor = arrOfColors[randomIndex2]
+        colorBox3.backgroundColor = arrOfColors[randomIndex3]
+
+        
+    }
+    
+    func addingColorsForArray(){
+        //Creating array of colors
+        self.arrOfColors.append(UIColor.red)
+        self.arrOfColors.append(UIColor.blue)
+        self.arrOfColors.append(UIColor.yellow)
+        self.arrOfColors.append(UIColor.orange)
+        self.arrOfColors.append(UIColor.gray)
+        self.arrOfColors.append(UIColor.white)
+        self.arrOfColors.append(UIColor.green)
+        self.arrOfColors.append(UIColor.purple)
+
+    }
+    
+    func cornerRadius(){
+        
+        colorBox1.layer.cornerRadius = 6
+        colorBox2.layer.cornerRadius = 6
+        colorBox3.layer.cornerRadius = 6
+
+    }
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        warning()
+    }
+    
     
     
 }
