8e57bee3b676f56f79379c60819c50d8b3c290df
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-26 15:37:26 -0400

    Visual setting added

---
+++ Light DJ/VisualizerSettingViewController.swift
@@ -0,0 +1,60 @@
+//
+//  VisualizerSettingViewController.swift
+//  Light DJ
+//
+//  Created by Zabeehullah Qayumi on 9/26/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class VisualizerSettingViewController: UIViewController {
+    
+    
+@IBOutlet var topThreeButtons: [UIButton]!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func garbageButtonPressed(_ sender: Any) {
+        warning()
+        
+    }
+
+    func warning(){
+        
+        let alert = UIAlertController(title: "Lights Not Connected", message: "Could not connected to the a light network. Check your WiFi connection and that the bulbs can be controlled through their official apps", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .default) { (action) in
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func visualizerButtonPressed(_ sender: Any) {
+        
+        topThreeButtons.forEach { (Button) in
+            UIView.animate(withDuration: 0.2, animations: {
+                Button.isHidden = !Button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    
+    @IBAction func visualizerSettingPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-Light DJ/Pods-Light DJ-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Light_DJ : NSObject
+@end
+@implementation PodsDummy_Pods_Light_DJ
+@end
