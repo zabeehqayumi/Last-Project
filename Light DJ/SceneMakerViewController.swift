a53daa0fb3cfaf1997fa338a5305c2a369733f4b
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-26 20:51:16 -0400

    Cells are designed Scenemaker

--- Light DJ/SceneMakerViewController.swift
+++ Light DJ/SceneMakerViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class SceneMakerViewController: UIViewController {
     
-    var arrOfCells : [UIImage] = []
+    var arrOfCells : [UIImage] = [#imageLiteral(resourceName: "strobeCycle"), #imageLiteral(resourceName: "partyStrobe"), #imageLiteral(resourceName: "softStrobe"), #imageLiteral(resourceName: "fillCycle"), #imageLiteral(resourceName: "partyStrobe"), #imageLiteral(resourceName: "impact"), #imageLiteral(resourceName: "doubeWave"),#imageLiteral(resourceName: "vortext"), #imageLiteral(resourceName: "drip"), #imageLiteral(resourceName: "drip"), #imageLiteral(resourceName: "blur"), #imageLiteral(resourceName: "fillCycle-2"), #imageLiteral(resourceName: "swagger"), #imageLiteral(resourceName: "quickFlash"), #imageLiteral(resourceName: "fireworks-1"), #imageLiteral(resourceName: "fadeCycle"), #imageLiteral(resourceName: "doubleFill"), #imageLiteral(resourceName: "swirl"), #imageLiteral(resourceName: "swirl"), #imageLiteral(resourceName: "flip"), #imageLiteral(resourceName: "strobeCycle"), #imageLiteral(resourceName: "flip"), #imageLiteral(resourceName: "grooveWave"), #imageLiteral(resourceName: "ascent"), #imageLiteral(resourceName: "split"), #imageLiteral(resourceName: "growCycle")]
     
     @IBOutlet var topThreeButtons: [UIButton]!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+
+        
         
 
         // Do any additional setup after loading the view.
@@ -73,6 +77,14 @@ extension SceneMakerViewController: UITableViewDelegate, UITableViewDataSource{
         cell.detailCells.image = arrOfCells[indexPath.row]
         return cell
         
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 55
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
     }
     
     
