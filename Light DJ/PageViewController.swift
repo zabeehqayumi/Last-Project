ac43b8020471fbdd6e4aa601fdf20b3db5668713
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-30 19:11:52 -0400

    pageController Modified

--- Light DJ/PageViewController.swift
+++ Light DJ/PageViewController.swift
@@ -8,20 +8,48 @@
 
 import UIKit
 
-class PageViewController: UIViewController {
+class PageViewController: UIViewController, UIScrollViewDelegate {
+    
+    var images : [String] = ["creatingEffects", "editingModes", "warningAndGhost"]
+    
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
 
-    @IBOutlet weak var pageController: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     @IBOutlet weak var pageController1: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+            pageController1.numberOfPages = images.count
+        
+        for index in 0..<images.count{
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imgView)
+            
+        }
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(images.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
 
-        // Do any additional setup after loading the view.
+
     }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageController1.currentPage = Int(pageNumber)
+    }
+    
+    
+    
     
 
     @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
